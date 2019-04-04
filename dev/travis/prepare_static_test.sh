b7fb5139943651b5e41a3799da375fe0091585ff
Sergii Gozhedrianov <sergii@deity.io>
2019-04-04 14:15:55 +0200

    updated static test setup, exlude magento test modules and exclude fixture files

--- dev/travis/prepare_static_test.sh
+++ dev/travis/prepare_static_test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 cp dev/travis/config/static_php_common.txt dev/tests/static/testsuite/Magento/Test/Php/_files/whitelist/common.txt
+cat dev/travis/config/phpcpd_blacklist.txt >> dev/tests/static/testsuite/Magento/Test/Php/_files/phpcpd/blacklist/common.txt
