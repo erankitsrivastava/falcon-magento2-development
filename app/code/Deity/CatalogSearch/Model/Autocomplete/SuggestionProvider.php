7d0ab27565e07fe35aebc7368ea9c20d897a46fa
Sergii Gozhedrianov <sergii@deity.io>
2019-04-24 13:54:06 +0200

    refactored suggestion provider

--- app/code/Deity/CatalogSearch/Model/Autocomplete/SuggestionProvider.php
+++ app/code/Deity/CatalogSearch/Model/Autocomplete/SuggestionProvider.php
@@ -5,12 +5,11 @@ namespace Deity\CatalogSearch\Model\Autocomplete;
 
 use Deity\CatalogSearchApi\Api\Data\AutocompleteItemInterfaceFactory as ItemFactory;
 use Deity\CatalogSearchApi\Model\Autocomplete\DataProviderInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface as ScopeConfig;
-use Magento\Framework\Stdlib\StringUtils as StdlibString;
-use Magento\Search\Helper\Data;
-use Magento\Search\Model\Query;
-use Magento\Search\Model\QueryFactory;
-use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\State;
+use Magento\Search\Model\AutocompleteInterface;
+use Magento\Search\Model\AutocompleteInterfaceFactory;
 
 /**
  * Class SuggestionProvider
@@ -19,19 +18,17 @@ use Magento\Store\Model\ScopeInterface;
  */
 class SuggestionProvider implements DataProviderInterface
 {
-    /**
-     * Autocomplete limit
-     */
-    const CONFIG_AUTOCOMPLETE_LIMIT = 'catalog/search/autocomplete_limit';
-
     public const AUTOCOMPLETE_TYPE_SUGGESTION = 'suggestion';
 
     /**
-     * Query factory
-     *
-     * @var QueryFactory
+     * @var State
      */
-    private $queryFactory;
+    private $state;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
 
     /**
      * Autocomplete result item factory
@@ -41,142 +38,60 @@ class SuggestionProvider implements DataProviderInterface
     private $itemFactory;
 
     /**
-     * Limit
-     *
-     * @var int
+     * @var AutocompleteInterfaceFactory
      */
-    private $limit;
+    private $autocompleteFactory;
 
     /**
-     * @var Query
-     */
-    private $query;
-
-    /**
-     * @var StdlibString
-     */
-    private $string;
-
-    /**
-     * @var Data
-     */
-    private $queryHelper;
-
-    /**
-     * @param QueryFactory $queryFactory
      * @param ItemFactory $itemFactory
-     * @param ScopeConfig $scopeConfig
-     * @param StdlibString $string
-     * @param Data $queryHelper
+     * @param State $state
+     * @param Context $context
+     * @param AutocompleteInterfaceFactory $autocompleteFactory
      */
     public function __construct(
-        QueryFactory $queryFactory,
         ItemFactory $itemFactory,
-        ScopeConfig $scopeConfig,
-        StdlibString $string,
-        Data $queryHelper
+        State $state,
+        Context $context,
+        AutocompleteInterfaceFactory $autocompleteFactory
     ) {
-        $this->string = $string;
-        $this->queryHelper = $queryHelper;
-        $this->queryFactory = $queryFactory;
+        $this->autocompleteFactory = $autocompleteFactory;
+        $this->state = $state;
+        $this->request = $context->getRequest();
         $this->itemFactory = $itemFactory;
-
-        $this->limit = (int) $scopeConfig->getValue(
-            self::CONFIG_AUTOCOMPLETE_LIMIT,
-            ScopeInterface::SCOPE_STORE
-        );
     }
 
     /**
      * @param string $query
      * @return \Deity\CatalogSearchApi\Api\Data\AutocompleteItemInterface[]
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws \Exception
      */
     public function getAutocompleteItemsForQuery(string $query): array
     {
-        $collection = $this->get($query)->getSuggestCollection();
-        $query = $this->queryFactory->get()->getQueryText();
         $result = [];
-        foreach ($collection as $item) {
-            $resultItem = $this->itemFactory->create([
-                'title' => $item->getQueryText(),
-                'url' => $item->getQueryText(),
+        if (!$this->request->getParam('q', false)) {
+            return $result;
+        }
+
+        $magentoAutocompleteItems = $this->state->emulateAreaCode(
+            'frontend',
+            function (AutocompleteInterface $autocompleteObject) {
+                $responseData = [];
+                foreach ($autocompleteObject->getItems() as $resultItem) {
+                    $responseData[] = $resultItem->toArray();
+                }
+                return $responseData;
+            },
+            [$this->autocompleteFactory->create()]
+        );
+
+
+        foreach ($magentoAutocompleteItems as $item) {
+            $result[] = $this->itemFactory->create([
+                'title' => $item['title'],
+                'url' => $item['title'],
                 'type' => self::AUTOCOMPLETE_TYPE_SUGGESTION
             ]);
-            if ($resultItem->getTitle() == $query) {
-                array_unshift($result, $resultItem);
-            } else {
-                $result[] = $resultItem;
-            }
         }
-        return ($this->limit) ? array_splice($result, 0, $this->limit) : $result;
-    }
-
-    private function get(string $query)
-    {
-        if (!$this->query) {
-            $maxQueryLength = $this->queryHelper->getMaxQueryLength();
-            $minQueryLength = $this->queryHelper->getMinQueryLength();
-            $rawQueryText = $this->getRawQueryText($query);
-            $preparedQueryText = $this->getPreparedQueryText($rawQueryText, $maxQueryLength);
-            $query = $this->queryFactory->create()->loadByQueryText($preparedQueryText);
-            if (!$query->getId()) {
-                $query->setQueryText($preparedQueryText);
-            }
-            $query->setIsQueryTextExceeded($this->isQueryTooLong($rawQueryText, $maxQueryLength));
-            $query->setIsQueryTextShort($this->isQueryTooShort($rawQueryText, $minQueryLength));
-            $this->query = $query;
-        }
-        return $this->query;
-    }
-
-    /**
-     * Retrieve search query text
-     *
-     * @param string $queryText
-     * @return string
-     */
-    private function getRawQueryText(string $queryText)
-    {
-        return ($queryText === null || is_array($queryText))
-            ? ''
-            : $this->string->cleanString(trim($queryText));
-    }
-
-    /**
-     * @param string $queryText
-     * @param int|string $maxQueryLength
-     * @return string
-     */
-    private function getPreparedQueryText($queryText, $maxQueryLength)
-    {
-        if ($this->isQueryTooLong($queryText, $maxQueryLength)) {
-            $queryText = $this->string->substr($queryText, 0, $maxQueryLength);
-        }
-        return $queryText;
-    }
-
-    /**
-     * Check if query is Too Long
-     *
-     * @param string $queryText
-     * @param int|string $maxQueryLength
-     * @return bool
-     */
-    private function isQueryTooLong($queryText, $maxQueryLength)
-    {
-        return ($maxQueryLength !== '' && $this->string->strlen($queryText) > $maxQueryLength);
-    }
-
-    /**
-     * Check if query is too short
-     *
-     * @param string $queryText
-     * @param int|string $minQueryLength
-     * @return bool
-     */
-    private function isQueryTooShort($queryText, $minQueryLength)
-    {
-        return ($this->string->strlen($queryText) < $minQueryLength);
+        return $result;
     }
 }
