36d038de791fa37b4a59522ce858277d4cdc7970
Sergii Gozhedrianov <sergii@deity.io>
2019-04-24 16:51:37 +0200

    introduced functional tests for search autocomplete

--- app/code/Deity/CatalogSearchApi/Test/Api/SearchAutocompleteTest.php
+++ app/code/Deity/CatalogSearchApi/Test/Api/SearchAutocompleteTest.php
@@ -30,4 +30,27 @@ class SearchAutocompleteTest extends WebapiAbstract
 
         $this->assertEquals(3, count($response), 'Three items expected to be returned');
     }
+
+    /**
+     * @magentoApiDataFixture ../../../../app/code/Deity/CatalogSearchApi/Test/_files/searchable_products.php
+     * @magentoApiDataFixture ../../../../app/code/Deity/CatalogSearchApi/Test/_files/search_reindex.php
+     */
+    public function testProductReturnFields()
+    {
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => str_replace('#query', 'shoe', self::AUTOCOMPLETE_REST_PATH),
+                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET,
+            ]
+        ];
+
+        $response = $this->_webApiCall($serviceInfo);
+        $this->assertEquals(1, count($response), 'One item expected to be returned');
+
+        $productInfo = $response[0];
+
+        $this->assertEquals('Simple Shoe', $productInfo['title']);
+        $this->assertEquals('simple-shoe.html', $productInfo['url_path']);
+        $this->assertEquals('product', $productInfo['type']);
+    }
 }
